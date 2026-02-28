68bf3f78fb8ce1706aca7e0e245c5487e9e03bfd
RayaneBarrosM <rayane.mendes@fatec.sp.gov.br>
2026-02-27 22:22:51 -0300

    Projeto 1

---
+++ ButtonTextView/README.md
@@ -0,0 +1,20 @@
+# Aula de Programação para Dispositivos Móveis (fevereiro/2026)
+
+🔎 Este repositório contém os códigos/atividades desenvolvidas nas aulas de Programação para Dispositivos Móveis sobre Button e Text View.
+
+## Instalação
+
+📌 Instale o projeto com npm:
+
+```bash
+  npm install ButtonTextView
+  cd ButtonTextView
+```
+    
+## Autores
+
+- [@Natali](https://github.com/nouveauromance)
+
+🔗 Aula por Vinícius Heltai Pacheco
+
+📍 Fatec Diadema Luigi Papaiz 
---
+++ ButtonTextView/app/src/main/java/br/gov/sp/cps/buttontext/MainActivity.java
@@ -0,0 +1,69 @@
+package br.gov.sp.cps.buttontext;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private TextView[] viewResultados; //Lista onde aparece os numeros
+
+    public void sorteiaNumero(View view){
+        //vincula o elemento textResultado(visualização) com o MainActivity:
+        viewResultados = new TextView[] {
+                findViewById(R.id.textResultado1),
+                findViewById(R.id.textResultado2),
+                findViewById(R.id.textResultado3),
+                findViewById(R.id.textResultado4),
+                findViewById(R.id.textResultado5),
+                findViewById(R.id.textResultado6)
+        };
+
+        //lista que guarda os numeros
+        ArrayList<Integer> nmSorteados = new ArrayList<>();
+        Random random = new Random();
+
+        //lógica de números aleatórios
+        while (nmSorteados.size() == 6){
+            int n = random.nextInt(60) + 1;
+
+            if(!nmSorteados.contains(n)){ //Se não conter na lista
+                nmSorteados.add(n);
+            }
+        }
+
+        //Adiciona os numeros ao textView
+        for (int i = 0; i < viewResultados.length; i++) {
+            viewResultados[i].setText(String.valueOf(nmSorteados.get(i)));
+        }
+    }
+
+    //Apagar numeros
+    public void apagaNumeros(View view){
+        for (int i = 0; i < viewResultados.length; i++) {
+            viewResultados[i].setText("");
+        }
+    }
+
+}
